85b514f05d2c044536975a905c7869b5d83e2abb
daniel-lee-user <danielhlee02@gmail.com>
2022-02-03 00:24:40 -0800

    Fixed failing test

--- AdditionClassTest.java
+++ AdditionClassTest.java
@@ -1,5 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 public class AdditionClassTest {
-    // need to think of tests to run + import junit libraries
+    @Test
+    public void testAdd() {
+        assertEquals(5, AdditionClass.add(1,4));
+        // should pass because 5 = 5
+        // 1 failure 1 test, time to commit and push
+        // time  to run on ssh server
+        // fails all the same.
+        // time to change the test so it passes
+    }
 }
